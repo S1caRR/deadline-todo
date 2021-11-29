9afc721a9d04f82c497bb286ad6202229c747652
S1caRR <frozendanoff@gmail.com>
2021-11-29 14:44:25 +0500

    Add aiohttp_cors

--- server/deadline_todo/app.py
+++ server/deadline_todo/app.py
@@ -3,12 +3,19 @@ from routes.auth import auth_router
 from routes.task import task_router
 
 from aiohttp import web
+import aiohttp_cors
 
 
 async def init_app() -> web.Application:
     app = web.Application(middlewares=[auth_middleware])
     app.add_routes(auth_router)
     app.add_routes(task_router)
+
+    cors = aiohttp_cors.setup(app, defaults={
+        '*': aiohttp_cors.ResourceOptions(allow_credentials=True, expose_headers="*", allow_headers="*")})
+    for route in app.router.routes():
+        cors.add(route)
+
     return app
 
 
