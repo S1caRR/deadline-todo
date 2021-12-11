77da384e3f7f2f584e6e92f2977b23a6f9d42b41
S1caRR <frozendanoff@gmail.com>
2021-12-12 04:59:26 +0500

    Add startup and cleanup for scheduler and bot

--- server/deadline_todo/app.py
+++ server/deadline_todo/app.py
@@ -6,9 +6,21 @@ from aiohttp import web
 import aiohttp_cors
 import logging
 
+from sheduler.send_tasks import scheduler_run, scheduler_close
+from extsystems.tg_bot import bot_start, bot_shutdown
+
 
 async def init_app() -> web.Application:
     app = web.Application(middlewares=[auth_middleware])
+
+    # Add telegram bot asyncio task
+    app.on_startup.append(bot_start)
+    app.on_cleanup.append(bot_shutdown)
+
+    # Add scheduler asyncio task
+    app.on_startup.append(scheduler_run)
+    app.on_cleanup.append(scheduler_close)
+
     app.add_routes(auth_router)
     app.add_routes(task_router)
 
@@ -26,4 +38,5 @@ async def init_app() -> web.Application:
     return app
 
 
-web.run_app(init_app(), host='127.0.0.1', port=8081)
+if __name__ == '__main__':
+    web.run_app(init_app(), host='localhost', port=8081)
